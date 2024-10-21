ed205ef5b4307c9d7f161192a0f2b556c159cf1a
Gadoskey <gadoski321@gmail.com>
2024-10-21 18:06:06 +0100

    solved tASK 1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,7 +3,8 @@
 -- A SQL script that creates a table users if it does not exist
 
 CREATE TABLE IF NOT EXISTS users (
-    id AUTOINCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
