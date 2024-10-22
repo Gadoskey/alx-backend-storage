f5a613e10ee6247a4e1029c4bf42f09e2bf2dd34
Gadoskey <gadoski321@gmail.com>
2024-10-22 08:07:31 +0100

    5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -8,8 +8,9 @@ CREATE TRIGGER reset_valid_email_after_update
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email THEN
-        SET valid_email = 0
+    IF NEW.email <> OLD.email
+    THEN
+        SET NEW.valid_email = 0
     END IF;
 END$$
 
