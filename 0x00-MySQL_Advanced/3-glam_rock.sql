3ed80538609468bc1317c39b57ec360747ae70b4
Gadoskey <gadoski321@gmail.com>
2024-10-22 07:05:57 +0100

    well i eilll come back to this

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,9 @@
---  lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+-- Author: Gadoskey
+-- File: 3-glam_rock.sql
+-- A SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+
+SELECT band_name, 
+       (IFNULL(split, 2022) - formed) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+WHERE main_style = 'Glam rock'
 ORDER BY lifespan DESC;
