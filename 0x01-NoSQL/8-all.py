43114da1fb8ca63e0feb478e344b8147fa1f184b
Gadoskey <gadoski321@gmail.com>
2024-10-22 23:31:33 +0100

    8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,17 +2,16 @@
 """
 Author: Gadoskey
 File: 8-all.py
-Descrioption: A Python function that lists all documents in a collection
+Description: A Python function that lists all documents in a collection
 """
 
 
 def list_all(mongo_collection):
     # A function that lists all documents in a collection
-    empty_list = []
     doc_list = mongo_collection.find()
 
     if doc_list.count() == 0:
         # Returns an empty list if no document in the collection
-        return empty_list
+        return []
 
     return doc_list
