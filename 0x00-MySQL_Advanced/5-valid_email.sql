5d713eed5ebd94ad763c19df310d66560e47eefb
Gadoskey <gadoski321@gmail.com>
2024-10-22 07:56:16 +0100

    task 5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,18 @@
+-- Author: Gadoskey
+-- File: 5-valid_email.sql
+-- SQL script to create a trigger that resets the valid_email attribute when the email is changed
+
+DELIMITER $$
+
+CREATE TRIGGER reset_valid_email_after_update
+AFTER UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        UPDATE users
+        SET valid_email = 0
+        WHERE id = NEW.id;
+    END IF;
+END$$
+
+DELIMITER ;
