a05643d450d077b55776c442978a7b5398d782b4
Gadoskey <gadoski321@gmail.com>
2024-10-26 06:53:27 +0100

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,3 +21,31 @@ class Cache:
     key = str(uuid4())
     self._redis.set(key, data)
     return key
+  
+  def get(self, key: str, fn: Optional[Callable]) -> Union[str, bytes, int, float]:
+    """ A get method that takes a key string argument and
+    an optional Callable argument named fn. It converts data
+    back to the desired format
+    """
+    value = self._redis.get(key)
+    if fn:
+      value = fn(value)
+    return value
+  
+  def get_str(self, key: str) -> str:
+        """
+        Automatically parametrizes Cache.get with the correct string function.
+        """
+        value = self._redis.get(key)
+        return value.decode('utf-8')
+
+  def get_int(self, key: str) -> int:
+      """
+      Automatically parametrizes Cache.get with the correct int function.
+      """
+      value = self._redis.get(key)
+      try:
+          value = int(value.decode('utf-8'))
+      except Exception:
+          value = 0
+      return value
